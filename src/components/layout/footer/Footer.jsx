09363ffc10ab68318e5327c163f13738bac119de
0xNaim <naimislam3545@gmail.com>
2022-10-20 01:12:14 +0600

    feat: footer section added

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,42 +1,12 @@
-import About from "./About";
-import styles from "./Footer.module.scss";
-import Help from "./Help";
-import FooterButton from "./FooterButton";
-import Services from "./Services";
-import SocialMedia from "./SocialMedia";
-import TermsCondition from "./Terms&cond";
-import { Grid } from "@mui/material";
+import { Box, Container, Toolbar } from '@mui/material';
+import styles from './Footer.module.scss';
 
 const Footer = () => {
   return (
-    <div className={styles.footer__wrapper}>
-      <div className={styles.upper_footer}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} md={3} lg={3}>
-            <FooterButton />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3} lg={3}>
-            <Services />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3} lg={3}>
-            <About />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3} lg={3}>
-            <Help />
-          </Grid>
-        </Grid>
-      </div>
-      <div className={styles.lower_footer}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6} lg={6} xl={6}>
-            <TermsCondition />
-          </Grid>
-          <Grid item xs={12} md={6} lg={6} xl={6}>
-            <SocialMedia />
-          </Grid>
-        </Grid>
-      </div>
-    </div>
+    <Box component='footer' className={styles.footer__wrapper}>
+      <Toolbar>Naim</Toolbar>
+      {/* <Container maxWidth='xl'>Naim</Container> */}
+    </Box>
   );
 };
 
