478db9d4e510303688093b67b0532d2f44ac271a
0xNaim <naimislam3545@gmail.com>
2022-10-22 20:17:25 +0600

    feat: category sidebar added

--- src/components/layout/sidebar/Sidebar.jsx
+++ src/components/layout/sidebar/Sidebar.jsx
@@ -1,13 +1,63 @@
-import { Box, Typography } from '@mui/material';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import {
+  Box,
+  Collapse,
+  List,
+  ListItemButton,
+  ListItemText,
+  Paper,
+} from '@mui/material';
+import { useState } from 'react';
+import { useGetCategoriesQuery } from '../../../services/categoriesApi';
 import styles from './Sidebar.module.scss';
 
 const Sidebar = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => setOpen(!open);
+
+  const {
+    data: { data },
+    isLoading,
+  } = useGetCategoriesQuery();
+
+  const categories = data.map((category) => category.attributes);
+
   return (
-    <Box component='div' className={styles.sidebar}>
-      {['fsfds', 'fsdfds', 'sdfd'].map((item) => (
-        <Typography key={item}>{item}</Typography>
-      ))}
-    </Box>
+    <Paper component='div' className={styles.sidebar}>
+      <List>
+        {categories?.map((category) => {
+          const { sub_categories } = category;
+          const { data } = sub_categories;
+
+          return (
+            <Box component='div' key={category.slug}>
+              <ListItemButton disableRipple onClick={handleClick}>
+                <ListItemText primary={category.categoryName} />
+                {open ? <ExpandLess /> : <KeyboardArrowRightIcon />}
+              </ListItemButton>
+
+              <Collapse in={open} timeout='auto' unmountOnExit>
+                <List component='div' disablePadding>
+                  {data?.map((subCategory) => (
+                    <ListItemButton
+                      key={subCategory.slug}
+                      disableRipple
+                      sx={{ pl: 4 }}
+                    >
+                      <ListItemText
+                        primary={subCategory.attributes.subCategoryName}
+                      />
+                    </ListItemButton>
+                  ))}
+                </List>
+              </Collapse>
+            </Box>
+          );
+        })}
+      </List>
+    </Paper>
   );
 };
 
