ce9e9f5f14f4e22ad9d2dab91244cdeab5815195
shakilmahammud <hshakil987@gmail.com>
2022-10-31 10:14:35 +0600

    feat: login and register validation and api implement done

---
+++ src/redux/features/auth/authApi.js
@@ -0,0 +1,68 @@
+import { apiSlice } from "../api/apiSlice";
+import { userLoggedIn } from "./authSlice";
+
+export const authApi = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        userRegister: builder.mutation({
+            query: (data) => ({
+                url: "/register",
+                method: "POST",
+                body: data,
+            }),
+            async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+                try {
+                    const result = await queryFulfilled;
+
+                    localStorage.setItem(
+                        "auth",
+                        JSON.stringify({
+                            accessToken: result.data.accessToken,
+                            user: result.data.user,
+                        })
+                    );
+
+                    dispatch(
+                        userLoggedIn({
+                            accessToken: result.data.accessToken,
+                            user: result.data.user,
+                        })
+                    );
+                } catch (err) {
+                    // do nothing
+                }
+            },
+        }),
+        login: builder.mutation({
+            query: (data) => ({
+                url: "/login",
+                method: "POST",
+                body: data,
+            }),
+
+            async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+                try {
+                    const result = await queryFulfilled;
+
+                    localStorage.setItem(
+                        "auth",
+                        JSON.stringify({
+                            accessToken: result.data.accessToken,
+                            user: result.data.user,
+                        })
+                    );
+
+                    dispatch(
+                        userLoggedIn({
+                            accessToken: result.data.accessToken,
+                            user: result.data.user,
+                        })
+                    );
+                } catch (err) {
+                    // do nothing
+                }
+            },
+        }),
+    }),
+});
+
+export const { useLoginMutation, useUserRegister } = authApi;
