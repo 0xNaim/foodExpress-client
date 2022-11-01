54d0b313306a592f937f5e80d570524bc56a9e2a
0xNaim <naimislam3545@gmail.com>
2022-11-02 02:22:24 +0600

    fix: product details page some issue fixed

--- src/components/product-details/ProductDetails.jsx
+++ src/components/product-details/ProductDetails.jsx
@@ -1,25 +1,24 @@
-import { Box, Button, Grid, Typography } from "@mui/material";
-import { Container } from "@mui/system";
-import Image from "next/image";
-import React from "react";
-import { useState } from "react";
+import { Box, Button, Grid, Typography } from '@mui/material';
+import { Container } from '@mui/system';
+import Image from 'next/image';
+import React, { useState } from 'react';
 
-import styles from "./productDetails.module.css";
+import styles from './productDetails.module.css';
 
 const data = [
-  "Origin: Bangladesh",
+  'Origin: Bangladesh',
 
-  "Gentle, alcohol-free formula.",
+  'Gentle, alcohol-free formula.',
 
-  "Kills 99.9% of bacteria",
+  'Kills 99.9% of bacteria',
 
-  "Pre-moistured with water",
+  'Pre-moistured with water',
 
-  "Light-scented",
+  'Light-scented',
 
-  "Ingredients: Leaf juice, water, Disodium EDTA, glycerin, citric acid, benzalkonium chloride, sodium benzoate, polysorbate 20, aloe barbadensis, Vitamin E, Fragrance.",
+  'Ingredients: Leaf juice, water, Disodium EDTA, glycerin, citric acid, benzalkonium chloride, sodium benzoate, polysorbate 20, aloe barbadensis, Vitamin E, Fragrance.',
 
-  "Quantity: 80 Pcs.",
+  'Quantity: 80 Pcs.',
 ];
 
 const ProductDetails = () => {
@@ -37,30 +36,30 @@ const ProductDetails = () => {
   };
   return (
     <>
-      <Container maxWidth="xl" className={styles.wrapper}>
+      <Container maxWidth='xl' className={styles.wrapper}>
         <Grid container>
           <Grid items sm={12} md={12} lg={12}>
-            <Box component={"div"} className={styles.img_div}>
-              <Image src={"/assets/burger/7.jpg"} width={300} height={200} />
+            <Box component={'div'} className={styles.img_div}>
+              <Image src={'/assets/burger/7.jpg'} width={300} height={200} />
             </Box>
 
             <hr />
           </Grid>
 
           <Grid items sm={12} md={12} lg={12}>
-            <Box component="div" className={styles.lower_div}>
+            <Box component='div' className={styles.lower_div}>
               <Grid container>
                 <Grid items sm={12} md={7} lg={7}>
-                  <Box component={"div"} className={styles.lower_left}>
-                    <Box component="div" sx={{ marginBottom: "1.5rem" }}>
-                      <Typography variant="h4" fontWeight={500}>
+                  <Box component={'div'} className={styles.lower_left}>
+                    <Box component='div' sx={{ marginBottom: '1.5rem' }}>
+                      <Typography variant='h4' fontWeight={500}>
                         Soyabin Whole
                       </Typography>
                     </Box>
 
-                    {data.map((d) => (
-                      <Box component="div">
-                        <Typography variant="subtitle2" gutterBottom>
+                    {data.map((d, i) => (
+                      <Box key={i} component='div'>
+                        <Typography variant='subtitle2' gutterBottom>
                           {d}
                         </Typography>
                       </Box>
@@ -69,27 +68,27 @@ const ProductDetails = () => {
                 </Grid>
 
                 <Grid items sm={12} md={5} lg={5}>
-                  <Box component="div" className={styles.lower_right}>
-                    <Box component="div" sx={{ marginBottom: "1rem" }}>
-                      <Typography variant="h4" color="primary">
+                  <Box component='div' className={styles.lower_right}>
+                    <Box component='div' sx={{ marginBottom: '1rem' }}>
+                      <Typography variant='h4' color='primary'>
                         ৳ {200 * productCount}
                       </Typography>
                     </Box>
 
-                    <Box component="div" className={styles.counter}>
+                    <Box component='div' className={styles.counter}>
                       <Button
-                        variant="contained"
-                        color="primary"
+                        variant='contained'
+                        color='primary'
                         onClick={handleDecrement}
                       >
                         -
                       </Button>
-                      <Typography variant="h6" color="HighlightText">
+                      <Typography variant='h6' color='HighlightText'>
                         {productCount}
                       </Typography>
                       <Button
-                        variant="contained"
-                        color="primary"
+                        variant='contained'
+                        color='primary'
                         onClick={handleIncrement}
                       >
                         +
