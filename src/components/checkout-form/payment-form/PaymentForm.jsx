5e837be8814b2fcada3a542445f0ec48a80e4c8e
0xNaim <naimislam3545@gmail.com>
2022-11-02 03:53:50 +0600

    refactor: checkout form code updated

--- src/components/checkout-form/payment-form/PaymentForm.jsx
+++ src/components/checkout-form/payment-form/PaymentForm.jsx
@@ -3,19 +3,22 @@ import CustomButton from '../../ui/Button/CustomButton';
 import ReviewItems from '../ReviewItems';
 import styles from './PaymentForm.module.scss';
 
-const PaymentForm = ({ backStep }) => (
-  <Box component='div'>
-    <ReviewItems />
-    <Divider />
+const PaymentForm = ({ backStep }) => {
+  return (
+    <Box component='div'>
+      <ReviewItems />
+      <Divider />
 
-    <Box className={styles.paymentForm__btnGroup} component='div'>
-      <CustomButton
-        label={'Back'}
-        handleClick={backStep}
-        variant='outlined' />
-      <CustomButton label={'Pay'} />
+      <Box className={styles.paymentForm__btnGroup} component='div'>
+        <CustomButton
+          label={'Back'}
+          handleClick={backStep}
+          variant='outlined'
+        />
+        <CustomButton label={'Pay'} />
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 export default PaymentForm;
