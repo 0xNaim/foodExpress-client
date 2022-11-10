1c605a8625a53a84e7ed27d5be78ba4d2e28f49f
0xNaim <naimislam3545@gmail.com>
2022-11-11 01:30:42 +0600

    feat: PWA added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -7,4 +11,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
